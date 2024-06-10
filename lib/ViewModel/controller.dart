e027e82fcc2300dbcbdc1434cc2647cf74bb5841
Nurujjaman329 <mdnurujjaman329@gmail.com>
2024-06-10 16:20:47 +0600

    mvvm structure

---
+++ lib/ViewModel/controller.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_todo_app/Model/model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TaskViewModel extends GetxController {
+  final TextEditingController controller = TextEditingController();
+  final RxList<Task> tasks = <Task>[].obs;
+  int currentPriority = 0;
+
+  void loadTasks() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String>? taskList = prefs.getStringList('tasks');
+
+    if (taskList != null) {
+      tasks.assignAll(
+          taskList.map((jsonString) => Task.fromJson(jsonDecode(jsonString))));
+    }
+  }
+
+  void saveTask() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<Map<String, dynamic>> taskList =
+        tasks.map((task) => task.toJson()).toList();
+
+    prefs.setStringList(
+        'tasks', taskList.map((json) => jsonEncode(json)).toList());
+  }
+
+  void addTask({required String taskTitle}) {
+    currentPriority++;
+    if (taskTitle.isNotEmpty) {
+      tasks.add(Task(title: taskTitle, priority: currentPriority));
+      controller.clear();
+      saveTask();
+    }
+  }
+
+  void deleteTask(int index) {
+    tasks.removeAt(index);
+    saveTask();
+  }
+
+  void toggleTaskComplete(int index) {
+    Task task = tasks[index];
+
+    if (task.dueDate != null && task.dueDate!.isBefore(DateTime.now())) {
+      task.isCompleted = true;
+    } else {
+      task.isCompleted = !task.isCompleted;
+    }
+    tasks[index] = task;
+    saveTask();
+  }
+
+  void setTaskDueDate(int index, DateTime? dueDate) {
+    Task task = tasks[index];
+    task.dueDate = dueDate;
+    tasks[index] = task;
+    saveTask();
+  }
+
+  void setTaskPriority(int index, int priority) {
+    Task task = tasks[index];
+    task.priority = priority;
+    tasks[index] = task;
+    saveTask();
+  }
+}
